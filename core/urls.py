c6829072b0cbd590a0d832f6562c5b2161bf6c59
viniciusantunes26 <viniciusantunes29@gmail.com>
2023-09-22 21:06:01 -0300

    documentado arquivo URLS

--- core/urls.py
+++ core/urls.py
@@ -14,24 +14,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# Importações necessárias do Django
 from django.contrib import admin
 from django.urls import path
 
+# Importações das visualizações personalizadas do aplicativo
 from school.views.studentView import StudentView
 from school.views.studentList import StudentListView
-
 from school.views.disciplineView import DisciplineView
 from school.views.disciplineList import DisciplineListView
-
 from school.views.taskView import TaskView
 from school.views.taskList import TaskListView
 
+# Lista de urlpatterns que define as rotas da API
 urlpatterns = [
+    # Rota para a área administrativa do Django
     path('api/admin/', admin.site.urls),
+    # Rotas para as visualizações relacionadas a estudantes (students)
     path('api/students/', StudentView.as_view(), name='student-list'),
     path('api/students/<int:pk>/', StudentListView.as_view(), name='student-detail'),
+    # Rotas para as visualizações relacionadas a disciplinas (disciplines)
     path('api/disciplines/', DisciplineView.as_view(), name='discipline-list'),
     path('api/disciplines/<int:pk>/', DisciplineListView.as_view(), name='discipline-detail'),
+    # Rotas para as visualizações relacionadas a tarefas (tasks)
     path('api/tasks/', TaskView.as_view(), name='task-list'),
     path('api/tasks/<int:pk>/', TaskListView.as_view(), name='task-detail'),
 ]
